86049551f1be0a00dc3d17547a9e942d5d918b58
Laura <laura.e.vollmer@gmail.com>
2024-08-13 10:48:37 -0500

    setting up the components in the app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Landing from "./components/Landing";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Projects from "./components/Projects";
+
 
 function App() {
   return (
     <>
       <div>
-        Hello
+        <Navigation />
+        <Landing />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
       </div>
     </>
   );
