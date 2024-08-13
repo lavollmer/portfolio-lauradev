ff42ed6398a9e64a1b38429e56a7258356f3623d
Laura <laura.e.vollmer@gmail.com>
2024-08-13 10:54:29 -0500

    added colors to tailwind config

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { IoMdFlower } from "react-icons/io";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <div className="">
+      <div>
+        <IoMdFlower />
+      </div>
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        seashell: "#fcf3f0ff",
+        "white-smoke": "#f5f3f2ff",
+        white: "#ffffffff",
+        "tropical-indigo": "#aa9aebff",
+        platinum: "#e7e2e0ff",
+        "antiflash-white": "#ececf1ff",
+      },
+    },
   },
   plugins: [],
-}
+};
