a054c2c887df353554d7bed50f8d539006f377b4
Laura <laura.e.vollmer@gmail.com>
2024-08-15 09:40:22 -0500

    updating navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,17 +7,15 @@ import Logo from "../assets/logo.png";
 const Navigation = () => {
   return (
     <>
-      <div className="flex flex-row items-center justify-center bg-transparent fixed w-full top-0 left-0 z-10 text-xl text-white font-public-sans">
-        <div className="flex flex-row justify-between w-full max-w-screen-lg px-4">
-          <div className="flex flex-row items-center">
-            <img src={Logo} alt="flower Logo" className="h-10 w-70" />
-            <h1>LAURA VOLLMER</h1>
-          </div>
-          <div>
-            <ul className="flex flex-row space-x-4">
-              <li>About</li>
-              <li>Projects</li>
-              <li>Contact</li>
+      <div className="flex flex-row items-center justify-center bg-white fixed w-full top-0 left-0 z-10 text-xl text-black font-public-sans">
+        <div className="flex flex-row justify-between w-full">
+          <div className="flex flex-row items-center space-x-4">
+            <img src={Logo} alt="flower Logo" className="h-32 w-32" />
+            <h1 className="font-bold">LAURA VOLLMER</h1>
+            <ul className="flex flex-row space-x-6">
+              <li>ABOUT</li>
+              <li>PROJECTS</li>
+              <li>CONTACT</li>
             </ul>
           </div>
           <div className="flex flex-row items-center space-x-2">
