3b873497eafe73eb75c7bb72dcddd8b1a3f194f4
Laura <laura.e.vollmer@gmail.com>
2024-08-15 09:49:13 -0500

    using old code from another project for navigation bar and updating with the style I want

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,59 @@
 import React from "react";
-import { FaLinkedin } from "react-icons/fa";
-import { FaSquareGithub } from "react-icons/fa6";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 import Logo from "../assets/logo.png";
+import SideNavBar from "./SideNav";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeSideNavBar = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <>
-      <div className="flex flex-row items-center justify-center bg-white fixed w-full top-0 left-0 z-10 text-xl text-black font-public-sans">
-        <div className="flex flex-row justify-between w-full">
-          <div className="flex flex-row items-center space-x-4">
-            <img src={Logo} alt="flower Logo" className="h-32 w-32" />
-            <h1 className="font-bold">LAURA VOLLMER</h1>
-            <ul className="flex flex-row space-x-6">
-              <li>ABOUT</li>
-              <li>PROJECTS</li>
-              <li>CONTACT</li>
-            </ul>
-          </div>
-          <div className="flex flex-row items-center space-x-2">
-            <FaLinkedin />
-            <FaSquareGithub />
-          </div>
+    <div className="font-public-sans">
+      <div className="md:hidden flex flex-row fixed top-0 z-20 w-full justify-between items-center p-6 bg-white">
+        <div>
+          <img src={Logo} alt="Flower logo" />
+        </div>
+        {/* hamburger button click */}
+        <div>
+          <button onClick={handleOnClick}>
+            <img
+              src={isOpen ? <IoMdClose /> : <RxHamburgerMenu />}
+              alt="menu icon"
+            />
+          </button>
         </div>
       </div>
-    </>
+      <div className="flex flex-col">{isOpen && <SideNavBar />}</div>
+      <div className="hidden md:flex flex-row fixed top-0 z-20 w-full justify-between items-center bg-white font-public-sans p-8">
+        <div>
+          <img src={Logo} alt="Easybank logo" />
+        </div>
+        <div className="flex flex-row space-x-6 text-grayish-blue cursor-pointer">
+          <h1 className="hover:border-b-2 hover:border-lime-green">About</h1>
+          <h1 className="hover:border-b-2 hover:border-lime-green">Projects</h1>
+          <h1 className="hover:border-b-2 hover:border-lime-green">Contact</h1>
+        </div>
+        <div>
+          <button className="bg-gradient-to-r to-blue-400 from-green-400 rounded-full text-white cursor-pointer p-2 px-6 hover:from-blue-400 hover:to-green-300">
+            <FaLinkedin />
+          </button>
+          <button className="bg-gradient-to-r to-blue-400 from-green-400 rounded-full text-white cursor-pointer p-2 px-6 hover:from-blue-400 hover:to-green-300">
+            <FaGithubSquare />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Sidenav = () => {
+const SideNavBar = () => {
   return (
-    <div>Sidenav</div>
-  )
-}
+    <>
+      <div className="flex flex-col items-center justify-center bg-white text-black text-public-sans mt-20 font-semibold space-y-2 m-6">
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Contact</h1>
+      </div>
+    </>
+  );
+};
 
-export default Sidenav
+export default SideNavBar;
