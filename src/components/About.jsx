cd223b4e167275a7646462c47e66a2be73c2e360
Laura <laura.e.vollmer@gmail.com>
2024-08-16 09:15:21 -0500

    mobile section of about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,15 +3,15 @@ import Avatar from "../assets/Avatar.png";
 
 const About = () => {
   return (
-    <div className="flex flex-row items-center justify-center p-40 font-jost text-black">
-      <div className="flex flex-col w-1/2 pr-10 space-y-10">
+    <div className="flex flex-col md:flex-row items-center justify-center p-10 md:p-40 font-jost text-black">
+      <div className="flex flex-col w-full md:w-1/2 md:pr-10 space-y-10">
         <div>
           <h1 className="text-black text-5xl">About</h1>
         </div>
         <div className="flex flex-row">
-          <h1 className="text-3xl font-bold">Why hello there!</h1>
+          <h1 className="text-xl md:text-3xl font-bold">Why hello there!</h1>
         </div>
-        <div className="flex flex-col space-y-10 text-xl ">
+        <div className="flex flex-col space-y-10 text-lg md:text-xl ">
           <p>
             I enjoy learning and solving complex problems. Previously, I
             worked in healthcare at large academic medical centers and
