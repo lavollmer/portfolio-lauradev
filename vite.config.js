a5a4c70f6438510450f8ed6340fc6a5cbd82b197
Laura <laura.e.vollmer@gmail.com>
2024-08-16 12:35:14 -0500

    debugging netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.svg'], 
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
