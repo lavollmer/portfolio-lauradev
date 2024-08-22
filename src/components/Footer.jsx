ed0dfe55e32f76db519fa6d72cedce1bf8893a19
Laura <laura.e.vollmer@gmail.com>
2024-08-22 13:55:10 -0500

    updating the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-scroll";
 const Footer = () => {
   return (
     <div className="flex flex-col md:flex-row md:space-x-20 p-4 items-center text-white font-jost justify-center space-y-8 md:space-y-0 bg-black mt-20">
-      <div className="flex flex-row space-x-10">
+      <div className="flex flex-col items-center justify-center md:flex-row md:space-x-10 space-x-0 space-y-4 md:space-y-0 text-lg md:text-xl">
         <Link aria-label="Return to home" to="navigation" smooth={true} duration={500} href="#home">
           <button aria-label="home">HOME</button>
         </Link>
@@ -29,7 +29,7 @@ const Footer = () => {
           aria-label="Go to Laura Dev Linkedin Profile"
         >
           <button aria-label="linkedin" className="hover:animate-bounce bg-gradient-to-r to-redwood from-burnt-sienna rounded-full text-white cursor-pointer p-4 px-4 hover:from-redwood hover:to-burnt-sienna">
-            <FaLinkedin size={20} />
+            <FaLinkedin size={24} />
           </button>
         </a>
         <a
@@ -39,7 +39,7 @@ const Footer = () => {
           aria-label="Go to Laura Dev Github Profile"
         >
           <button aria-label="github" className="hover:animate-bounce bg-gradient-to-r to-redwood from-burnt-sienna rounded-full text-white cursor-pointer p-4 px-4  hover:from-redwood hover:to-burnt-sienna">
-            <FaGithubSquare size={20} />
+            <FaGithubSquare size={24} />
           </button>
         </a>
         <a
@@ -49,7 +49,7 @@ const Footer = () => {
           aria-label="Go to Laura Dev Frontend Mentor Profile"
         >
           <button aria-label="frontend mentor" className="hover:animate-bounce bg-gradient-to-r to-redwood from-burnt-sienna rounded-full text-white cursor-pointer p-4 px-4 hover:from-redwood hover:to-burnt-sienna">
-            <SiFrontendmentor size={20} />
+            <SiFrontendmentor size={24} />
           </button>
         </a>
       </div>
