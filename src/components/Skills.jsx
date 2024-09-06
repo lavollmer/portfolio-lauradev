e94b874289fce9ec24126c73a27c9dbbb38f749d
Laura <laura.e.vollmer@gmail.com>
2024-09-06 13:42:48 -0500

    styling page for ipad mini screen size skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,15 +3,15 @@ import SkillsCard from "./SkillsCard";
 
 const Skills = () => {
   return (
-    <div className="flex flex-col text-jost p-10 space-y-6 md:space-y-10 md:p-20 md:mt-10 md:space-x-2 rounded-lg">
+    <div className="flex flex-col text-jost p-10 space-y-6 md:space-y-8 lg:space-y-10 md:p-20 md:mt-10 md:space-x-2 rounded-lg">
       <div className="p-10">
         <hr />
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center pb-10">
-        <h1 className="text-black dark:text-white text-3xl md:text-5xl">Technical Skills</h1>
+        <h1 className="text-black dark:text-white text-3xl md:text-4xl lg:text-5xl">Technical Skills</h1>
       </div>
-      <div className="flex flex-col items-center w-full justify-center md:justify-evenly md:space-y-10 ">
-        <div className="flex flex-row items-center justify-center w-full lg:flex-row flex-wrap lg:space-x-4 space-y-6 lg:space-y-0">
+      <div className="flex flex-col items-center w-full justify-center lg:justify-evenly md:space-y-6 lg:space-y-10 ">
+        <div className="flex flex-col lg:flex-row items-center justify-center w-full flex-wrap lg:space-x-4 space-y-6 lg:space-y-0">
           <SkillsCard
             title="React"
             content="1 year of experience with React. I have built multiple projects using React, including this website."
@@ -22,7 +22,7 @@ const Skills = () => {
           />
           <SkillsCard title="JavaScript" content="1 year of experience" />
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center w-full mt-4 md:mt-0 lg:flex-row  lg:space-x-4 space-y-6 lg:space-y-0">
+        <div className="flex flex-col lg:flex-row items-center justify-center w-full mt-4 md:mt-0 lg:flex-row lg:space-x-4 space-y-6 lg:space-y-0">
           <SkillsCard title="HTML" content="1 year of experience" />
           <SkillsCard title="CSS" content="1 year of experience" />
           <SkillsCard title="Web Accessibility" content="1 year of experience" />
--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const SkillsCard = ({ title, content }) => {
   return (
-    <div className="flex flex-col rounded-lg shadow-lg bg-white dark:bg-gray-800 overflow-hidden font-jost w-full h-full w-[200px] h-[200px] md:w-[300px] md:h-[200px] lg:w-[400px] lg:h-[200px]">
-      <div className="p-6 md:p-8 items-center justify-center">
-        <h2 className="text-black dark:text-white md:text-3xl text-lg font-semibold">
+    <div className="flex flex-col rounded-lg shadow-lg bg-white dark:bg-gray-800 overflow-hidden font-jost w-full h-full w-[200px] h-[200px] md:w-[400px] md:h-[300px] lg:w-[400px] lg:h-[200px]">
+      <div className="p-6 lg:p-8 items-center justify-center">
+        <h2 className="text-black dark:text-white lg:text-3xl md:text-2xl text-lg font-semibold">
           {title}
         </h2>
-        <p className="text-black dark:text-white mt-2 text-sm md:text-md">{content}</p>
+        <p className="text-black dark:text-white mt-2 text-md md:text-xl">{content}</p>
       </div>
     </div>
   );
