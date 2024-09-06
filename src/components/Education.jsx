9fcd82b918fd7ac6fd156e41e5d3cde0a26c97f8
Laura <laura.e.vollmer@gmail.com>
2024-09-06 13:49:46 -0500

    styling the learning and education section for responsive screen sizes of ipad mini

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,17 +3,17 @@ import Learning from "./Learning";
 
 const Education = () => {
   return (
-    <div className="flex flex-col text-jost p-10 space-y-6 md:space-y-10 md:p-20 md:mt-10 md:space-x-2 rounded-lg">
+    <div className="flex flex-col text-jost p-10 space-y-6 lg:space-y-10 lg:p-20 lg:mt-10 lg:space-x-2 rounded-lg">
       <div className="p-10">
         <hr />
       </div>
       <div className="flex flex-col md:flex-row items-center justify-center">
-        <h1 className="flex items-center justify-center text-center text-black dark:text-white text-3xl p-10 md:text-5xl">
+        <h1 className="flex items-center justify-center text-center text-black dark:text-white text-3xl p-10 lg:text-5xl">
           Professional Education
         </h1>
       </div>
       <div className="flex flex-col items-center w-full justify-center md:justify-evenly md:space-y-10">
-        <div className="flex flex-row items-center justify-center w-full lg:flex-row flex-wrap lg:space-x-4 space-y-6 lg:space-y-0">
+        <div className="flex flex-col lg:flex-row items-center justify-center w-full lg:flex-row flex-wrap lg:space-x-4 space-y-6 lg:space-y-0">
           <Learning
             title="Full-stack Web Development Certificate"
             content="12 full-stack intensive weeks of coding bootcamp at University of Minnesota focused on technical programming skills, group projects, and individual projects"
@@ -28,11 +28,11 @@ const Education = () => {
           />
         </div>
         <div className="flex flex-col md:flex-row items-center justify-center ">
-          <h1 className="flex items-center justify-center text-center text-black dark:text-white text-3xl p-10 md:text-5xl">
+          <h1 className="flex items-center justify-center text-center text-black dark:text-white text-3xl p-10 lg:text-5xl">
             Continous Web Development and Coding Experience
           </h1>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center w-full mt-4 md:mt-0 lg:flex-row  lg:space-x-4 space-y-6 lg:space-y-0">
+        <div className="flex flex-col lg:flex-row  items-center justify-center w-full mt-4 md:mt-0 lg:flex-row  lg:space-x-4 space-y-6 lg:space-y-0">
           <Learning
             title="Frontend Mentor Challenges"
             content="Frontend Mentor offers real-world web development challenges, sharpening HTML, CSS, and JavaScript skills. The structured feedback, solutions, and vibrant community have significantly impacted my learning and growth."
--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Learning = ({ title, content }) => {
   return (
-    <div className="flex flex-col rounded-lg shadow-lg bg-white dark:bg-gray-800 overflow-hidden font-jost w-full h-full w-[200px] h-[200px] md:w-[300px] md:h-[200px] lg:w-[500px] lg:h-[300px]">
-      <div className="p-6 md:p-8 items-center justify-center">
-        <h2 className="flex text-black dark:text-white md:text-3xl text-lg font-semibold">
+    <div className="flex flex-col rounded-lg shadow-lg bg-white dark:bg-gray-800 overflow-hidden font-jost w-full h-full w-[200px] h-[200px] md:w-[400px] md:h-[300px] lg:w-[500px] lg:h-[300px]">
+      <div className="p-6 lg:p-8 items-center justify-center">
+        <h2 className="flex text-black dark:text-white lg:text-3xl md:text-xl text-lg font-semibold">
           {title}
         </h2>
-        <p className="text-black dark:text-white mt-2 text-lg md:text-xl">{content}</p>
+        <p className="text-black dark:text-white mt-2 text-lg md:text-xl lg:text-xl">{content}</p>
       </div>
     </div>
   );
