8aceccf584e1db9c84b121f281d6664820005c38
Laura <laura.e.vollmer@gmail.com>
2025-06-07 00:27:16 -0500

    Updated my information and project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -45,7 +45,7 @@ const Projects = () => {
             mobileImage={MedProMobile}
             imageDesc="MedPronunciationAI App"
             title="MedPronunciationAI App"
-            content="As a solo developer, I created a full-featured React and Python web project integrating a navigation bar, hero section, call-to-action, footer, and enhanced with Google Text-to-Speech AI."
+            content="I created a full-featured React and Python web project integrating a navigation bar, hero section, call-to-action, footer, and enhanced with Google Text-to-Speech AI."
             skillOne="REACTJS"
             skillTwo="TAILWIND CSS"
             skillThree="VITE"
