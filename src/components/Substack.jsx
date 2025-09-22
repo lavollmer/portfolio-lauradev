4bd148ce19240ba1b05781b7c9eb23204f21afd0
Laura <laura.e.vollmer@gmail.com>
2025-09-22 12:33:53 -0500

    Updating the button without a Link and using an a tag

--- src/components/Substack.jsx
+++ src/components/Substack.jsx
@@ -12,20 +12,15 @@ const Substack = () => {
                 <p className='flex flex-col text-black dark:text-white text-xl'>Welcome to my Substack, where I dive deep into the world of coding and programming with clear, practical lessons designed for learners at all levels. Whether you’re just starting out or looking to sharpen your skills, my weekly posts break down complex concepts into easy-to-understand tutorials, real-world examples, and hands-on projects. Join a growing community of passionate developers and elevate your programming journey with tips, best practices, and insights that help you build confidence and code smarter every day.</p>
             </div>
             <div className='flex flex-col justify-content'>
-                <Link
-                    aria-label="Go to Learn More about Laura Dev"
-                    to="skills"
-                    smooth={true}
-                    duration={500}
-                    href="https://lauradev1.substack.com/?utm_campaign=profile&utm_medium=profile-page"
+                <button
+                    aria-label="read my substack"
+                    className="flex flex-row items-center justify-center space-x-2 bg-white animate-bounce rounded-full text-black text-xl cursor-pointer p-4 px-4 hover:from-redwood hover:to-burnt-sienna"
                 >
-                    <button
-                        aria-label="read my substack"
-                        className="flex flex-row items-center justify-center space-x-2 bg-white animate-bounce rounded-full text-black text-xl cursor-pointer p-4 px-4 hover:from-redwood hover:to-burnt-sienna"
-                    >
+                    <a href="https://lauradev1.substack.com/?utm_campaign=profile&utm_medium=profile-page">
                         <p>Read My Substack</p>
-                    </button>
-                </Link>
+                    </a>
+
+                </button>
             </div>
         </div>
     )
